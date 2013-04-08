a2c0f20fee05d23266782ee9ace8a07749818152
Dennis Flanigan <dflanigan@rallydev.com>
2013-04-08 13:17:01 -0600

    Connect example sets the WSAPI version to use.

--- examples/01-connect-to-rally.rb
+++ examples/01-connect-to-rally.rb
@@ -6,20 +6,15 @@ require 'pp'
 #Configuration for rally connection specified in 00_config.rb
 require_relative '00_config'
 
-
-
-puts "hello"
-
 begin
+  rally_connection = RallyAPI::RallyRestJson.new(@config)
 
   puts "rally_api version : #{RallyAPI::VERSION}"
-
-  rally_connection = RallyAPI::RallyRestJson.new(@config)
+  puts "Rally Web Services API version: #{rally_connection.wsapi_version}"
 
 rescue Exception=>boom
   puts "Rescued #{boom.class}"
   puts "Error Message: #{boom}"
 end
 
-puts "goodbye"
 
