158681c2ee7bb5b9a70a7fa4873d64388d5a5b77
Dennis Flanigan <dflanigan@rallydev.com>
2013-04-08 16:33:17 -0600

    Create artifact example

---
+++ examples/04-create-defect.rb
@@ -0,0 +1,36 @@
+#! /usr/bin/env ruby
+
+require 'rally_api'
+require 'pp'
+
+#Configuration for rally connection specified in 00_config.rb
+require_relative '00_config'
+
+def show_some_values(title, defect)
+  values = ["Name", "CreationDate", "FormattedID", "Priority"]
+  format = "%-12s : %s"
+
+  puts "-" * 80
+  puts title
+  values.each do |field_name|
+    puts format % [field_name, defect[field_name]]
+  end
+
+end
+
+
+begin
+  rally_connection = RallyAPI::RallyRestJson.new(@config)
+
+  fields = {}
+  fields["Name"] = "Test Defect created at #{Time.now.utc()} with Rally API gem"
+  fields["Priority"] = "High Attention"
+
+  new_defect = rally_connection.create("defect", fields)
+  show_some_values("New Defect Fields", new_defect)
+
+rescue Exception => boom
+  puts "Rescued #{boom.class}"
+  puts "Error Message: #{boom}"
+end
+
